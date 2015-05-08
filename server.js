cbd1922f9f41111a1a0a13db22f2f636f1a384ca
gabwatso <gabwatso@indiana.edu>
2015-05-08 19:03:38 -0400

    Update server.js
    First time using node.js, all code is internet based :( Couldnt get the google app to work but got a server up!

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 
+//makes a server couldnt get google working
+//First time using node.js :)
+var http = require( 'http' );
+/* 
+    google = require('google');
+google.resultsPerPage = 25;
+    var nextCounter = 0;
+ 
+var server = google('node.js best practices', function (err, next, links){
+	    if (err) console.error(err)
+ 
+			 for (var i = 0; i < links.length; ++i) {
+			     console.log(links[i].title + ' - ' + links[i].link) // link.href is an alias for link.link 
+			     console.log(links[i].description + "\n")
+			 }
+ 
+	    if (nextCounter < 4) {
+    nextCounter += 1
+    if (next) next()
+		  }
+    })
+*/
+var server = http.createServer(function (request, response) {  
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Gabi\'s server');
+    });
+server.listen( 8080 );
+console.log( "Webserver started." );
